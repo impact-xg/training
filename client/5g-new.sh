110e3320a13b5e96a7e2945f6204c72ee5d5407a
Nikos Fotiou <nikosft@gmail.com>
2025-06-05 11:32:58 +0300

    new videos

---
+++ client/5g-new.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SERVER_ADDRESS="195.251.234.25:8554"
+SERVER_RTP="195.251.234.25:8000"
+INPUT_PATH="/home/ubuntu/training/client/input"
+
+for s in 0 20 50; do
+    for i in {1..10}; do
+      BASE_SESSIONID=$(date +"%Y%m%d%H%M%S") 
+      SESSIONID="${BASE_SESSIONID}"
+      echo "...Starting repetition $i with SESSIONID: $SESSIONID"
+
+      # Start session
+      curl "http://$SERVER_ADDRESS/start/$SESSIONID/20"
+      sleep 2
+
+      # Stream video via ffmpeg
+      ~/bin/ffmpeg -re \
+        -i ${INPUT_PATH}/webcam-20kmh-part${s}.mp4 \
+        -input_format mjpeg \
+        -c copy \
+        -f rtp_mpegts "rtp://${SERVER_RTP}?pkt_size=1316"
+
+      # Stop session
+      curl http://${SERVER_ADDRESS}/stop
+
+      echo "Completed repetition $i"
+      echo "Sleeping for 5 seconds..."
+      sleep 5
+    done
+done
